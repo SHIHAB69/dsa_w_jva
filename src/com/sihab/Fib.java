6c1a8556e707d47b34cb0400fa87e8ea40fe2e14
SHIHAB69 <southcybersoldier@gmail.com>
2024-07-06 10:23:37 +0600

    n-2

---
+++ src/com/sihab/Fib.java
@@ -0,0 +1,11 @@
+package com.sihab;
+
+import java.util.Scanner;
+
+public class Fib {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+
+    }
+}
---
+++ src/com/sihab/Main.java
@@ -0,0 +1,11 @@
+package com.sihab;
+
+import  java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println(input.nextLine());
+    }
+}
