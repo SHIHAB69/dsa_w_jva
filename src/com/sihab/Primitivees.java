8d10b8bae776b117b6d107d53347069fe413e9fd
SHIHAB69 <southcybersoldier@gmail.com>
2024-07-15 23:02:52 +0600

    n-5

--- src/com/sihab/Primitivees.java
+++ src/com/sihab/Primitivees.java
@@ -4,14 +4,29 @@ import java.util.Scanner;
 
 public class Primitivees {
     public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = in.nextInt();
 
-       int num1 = input.nextInt();
-       int num2 = input.nextInt();
+        if (num % 2 == 0) {
+            System.out.println(num + " is an even number.");
+        } else {
+            System.out.println(num + " is an odd number.");
+        }
 
-       int sum = num1 + num2;
+        if (num > 0) {
+            System.out.println(num + " is a positive number.");
+        } else if (num < 0) {
+            System.out.println(num + " is a negative number.");
+        } else {
+            System.out.println(num + " is zero.");
+        }
 
-        System.out.println(sum);
+        if (num > 0 && num % 2 == 0) {
+            System.out.println(num + " is an even positive number.");
+        } else if (num < 0 && num % 2 == 0) {
 
+
+        }
     }
 }
