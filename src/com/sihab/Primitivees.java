5c6c569ec9ed6aa1521c71ea7834a95cca204ccd
SHIHAB69 <southcybersoldier@gmail.com>
2024-07-15 23:04:26 +0600

    n-6

--- src/com/sihab/Primitivees.java
+++ src/com/sihab/Primitivees.java
@@ -3,15 +3,15 @@ package com.sihab;
 import java.util.Scanner;
 
 public class Primitivees {
-    public static void main(String[] args) {
+    public static void main() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a number:");
         int num = in.nextInt();
 
         if (num % 2 == 0) {
-            System.out.println(num + " is an even number.");
+            System.out.println(STR."\{num} is an even number.");
         } else {
-            System.out.println(num + " is an odd number.");
+            System.out.println(STR."\{num} is an odd number.");
         }
 
         if (num > 0) {
@@ -24,9 +24,6 @@ public class Primitivees {
 
         if (num > 0 && num % 2 == 0) {
             System.out.println(num + " is an even positive number.");
-        } else if (num < 0 && num % 2 == 0) {
-
-
         }
     }
 }
