a01d68f6c6bba5c4ea380fdb96793f17245eb2f1
SHIHAB69 <southcybersoldier@gmail.com>
2024-07-15 23:06:10 +0600

    n-6

--- src/com/sihab/Main.java
+++ src/com/sihab/Main.java
@@ -4,10 +4,7 @@ import  java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Main obj = new Main();
-        obj.display(10); // calling display method of Main class with argument 10
-
-
+    Main obj = new Main();
     }
 void display(int d ){
     System.out.println(d);
