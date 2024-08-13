494f8aadd9d8c1876822b09c0ea3b04f3a440f5b
SHIHAB69 <southcybersoldier@gmail.com>
2024-08-13 23:30:37 +0600

    n-6

--- src/com/sihab/Arrays/swapArray.java
+++ src/com/sihab/Arrays/swapArray.java
@@ -1,16 +1,27 @@
 package com.sihab.Arrays;
 
+import java.util.Arrays;
+
 public class swapArray {
     public static void main(String[] args) {
-
         int[] array1 = {1, 2, 3, 4, 5};
-
-
-
+        swapArray(array1);
+        System.out.println(Arrays.toString(array1));
     }
-    static void swapArray(int[] array1, int[] array2) {
-        int[] temp = array1;
-        array1 = array2;
-        array2 = temp;
+
+    static void swapArray(int[] array1) {
+        int start = 0;
+        int end = array1.length - 1;
+        while (start < end) {
+            swap(array1, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
     }
 }
