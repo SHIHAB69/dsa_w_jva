ce43b45495def09f6c7ed76aad0efaf6316d4c9b
SHIHAB69 <southcybersoldier@gmail.com>
2024-08-13 23:25:26 +0600

    13 aug 24

---
+++ src/com/sihab/Arrays/swapArray.java
@@ -0,0 +1,16 @@
+package com.sihab.Arrays;
+
+public class swapArray {
+    public static void main(String[] args) {
+
+        int[] array1 = {1, 2, 3, 4, 5};
+
+
+
+    }
+    static void swapArray(int[] array1, int[] array2) {
+        int[] temp = array1;
+        array1 = array2;
+        array2 = temp;
+    }
+}
