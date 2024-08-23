962ff187596daadb95d15fcddf19d1aa694166bf
SHIHAB69 <southcybersoldier@gmail.com>
2024-08-23 23:34:33 +0600

    n-7

---
+++ src/com/sihab/Array.java
@@ -0,0 +1,13 @@
+package com.sihab;
+
+public class Array {
+    public static void main(String[] args) {
+        System.out.println("in this we are goinmy");
+    }
+    class Mevric{
+        Mevric(){
+            System.out.println("Objective");
+        }
+    }
+}
+
