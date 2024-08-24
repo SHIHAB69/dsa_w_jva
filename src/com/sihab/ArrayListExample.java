bf9d6a1c78b1eb72555590a297feaca8a2b0b8b5
SHIHAB69 <southcybersoldier@gmail.com>
2024-08-24 23:53:13 +0600

    n-8

--- src/com/sihab/ArrayListExample.java
+++ src/com/sihab/ArrayListExample.java
@@ -7,15 +7,7 @@ public class ArrayListExample {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         ArrayList<Integer> list = new ArrayList<Integer>(10);
-//        list.add(234);
-//        list.add(234);
-//        list.add(2324);
-//        list.add(2244);
-//        list.add(2343);
-//        list.add(233);
-//        System.out.println(list);
-//        list.set(0,99);
-//        System.out.println(list);
+
 
         for (int i = 0; i < 5; i++){
 
