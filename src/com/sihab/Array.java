a2189bfc32f197f3ce69008755583f5c772dcd67
SHIHAB69 <southcybersoldier@gmail.com>
2024-08-26 04:57:59 +0600

    Try 1

--- src/com/sihab/Array.java
+++ src/com/sihab/Array.java
@@ -1,10 +1,20 @@
 package com.sihab;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Array {
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
 
+        String[] str = new String[5];
 
+        for(int i  = 0; i<str.length; i++) {
+            str[i] = in.next();
+        }
+
+        System.out.println(Arrays.toString(str));
 
     }
 
