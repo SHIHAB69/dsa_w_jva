4cbb98c51d0e529e11dcf445e1c985876dbc07ba
SHIHAB69 <southcybersoldier@gmail.com>
2024-08-27 21:33:24 +0600

    Try 2

--- src/com/sihab/Array.java
+++ src/com/sihab/Array.java
@@ -3,6 +3,17 @@ package com.sihab;
 public class Array {
     public static void main(String[] args) {
 
+           // int[][] arr = new int[3][3];
+
+            int[][] arr = {
+
+                    {1,2,3},
+                    {4,5,6},
+                    {7,8,9}
+            };
+
+
+
 
 
 
