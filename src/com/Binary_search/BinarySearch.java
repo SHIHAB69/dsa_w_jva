e41f6ca52c891f668e0a9d716443bca397f25dbe
SHIHAB69 <southcybersoldier@gmail.com>
2024-10-05 22:36:33 +0600

    Linear search-2

--- src/com/Binary_search/BinarySearch.java
+++ src/com/Binary_search/BinarySearch.java
@@ -2,49 +2,31 @@ package com.Binary_search;
 
 
      class BinarySearch{
-         public class jigSaw {
-             int instance = 5;
-             static int check = 12;
-
-             public void puzzle1(){
-                 int rubics = 100;
-                 check = 15;
-                 System.out.println(rubics);
-                 System.out.println(instance);
-                 System.out.println(check);
-
-             }
+         public static void main(String[] args) {
+                int[] arr = {-1,0,3,5,6,12,21,22,43,44};
+                int target = 111 ;
+                int ans = binarysearch(arr, target);
+             System.out.println(ans);
          }
 
-         public class Sudoku extends jigSaw {
-             public void puzzle2(){
-                 int rubics = 200;
-                 instance = 9;
-                 System.out.println(rubics);
-                 System.out.println(instance);
-                 System.out.println(check);
-                 instance += 2;
-             }
-         }
+         static int binarysearch(int[] arr, int target){
 
-         public class Chess extends Sudoku{
-             public void puzzle2(){
-                 super.puzzle2();
-                 int rubics = 300;
-                 System.out.println(rubics);
-                 System.out.println(instance);
-                 System.out.println(check);
-                 instance += 2;
-             }
-         }
+             int start = 0;
+             int end = arr.length-1;
 
-         public void main(String[] args) {
-             Chess solve1 = new Chess();
-             Chess solve2 = new Chess();
+             while(start < end){
+                 int mid = start+(end - start) / 2;
 
-             solve1.puzzle1();
-             solve2.puzzle2();
-             System.out.println("Instance vvalue after puzzle2" + solve1.instance);
+                 if(target < arr[mid]){
+                     end = mid - 1;
+
+                 }else if (target > arr[mid]){
+                     start = mid + 1;
+                 }else {
+                     return mid;
+                 }
+              }
+             return -1;
 
          }
      }
---
+++ src/com/Binary_search/Print.java
@@ -0,0 +1,8 @@
+package com.Binary_search;
+
+public class Print {
+    public static void main(String[] args) {
+        int i2 = 50;
+        System.out.println(++i2);
+    }
+}
