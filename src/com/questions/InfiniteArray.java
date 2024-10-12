d1fe7bc07ae8080dd7414866f6bd86c087c0d27e
SHIHAB69 <southcybersoldier@gmail.com>
2024-10-12 22:06:04 +0600

    Infinite Array

---
+++ src/com/questions/InfiniteArray.java
@@ -0,0 +1,30 @@
+package com.questions;
+
+public class InfiniteArray {
+    public static void main(String[] args) {
+
+    }
+
+    static int ans(int[] arr, int target){
+        //find the range
+        //first start with a box of size 2
+        int start = 0;
+        int end = 1;
+
+        //condition for the target lie in the range
+        return -1;
+    }
+    static int binarySearch(int[] arr, int target , int start, int end){
+        while(start < end){
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]){
+                end = mid -1;
+            }else if(target > arr[mid]){
+                start = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
